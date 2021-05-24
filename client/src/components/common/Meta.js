ee67882d59706d5fdf3c0e870ec7ab713228082f
Danish Mehmood <danishm7012@gmail.com>
2021-05-24 17:10:57 +0500

    add meta data

--- client/src/components/common/Meta.js
+++ client/src/components/common/Meta.js
@@ -12,9 +12,11 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To Business and Learning Hub | Home',
-  description: 'We sell the best products for cheap',
-  keywords: 'real estate, import and export, shipping and logistics',
+  title: 'Welcome To AK-Ventures | Home',
+  description: `AKVentures is a group of companies which deals in al over the
+  United Arab Emirates and have following sister concern companies:
+  Al Manzal, Chiltern travel & tours, NAM Global and Trusticon.`,
+  keywords: 'real estate, import and export, tour and travel',
 }
 
 export default Meta
